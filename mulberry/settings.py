26fe4adfadeb0bb642ae1ba6a6e6230c6b32ade3
Melvin M T <melvinthankachan101@gmail.com>
2024-05-30 12:01:33 +0530

    remove superuser command

--- mulberry/settings.py
+++ mulberry/settings.py
@@ -80,6 +80,28 @@ WSGI_APPLICATION = "mulberry.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "ebdb",
+#         "USER": "mulberryDB",
+#         "PASSWORD": "Thankachan",
+#         "HOST": "awseb-e-udxburmppk-stack-awsebrdsdatabase-dn0do6huodi3.cr6eummm6ipo.us-west-2.rds.amazonaws.com",
+#         "PORT": 5432,
+#     }
+# }
+
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
