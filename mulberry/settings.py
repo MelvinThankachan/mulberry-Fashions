cc75a48491ec5a4fa715f758bbcc35d1710ce344
Melvin M T <melvinthankachan101@gmail.com>
2024-05-31 23:35:27 +0530

    Rollback to development condition

--- mulberry/settings.py
+++ mulberry/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-b)h3o2^!9-=@0fq+-2qzwfnk5vr2fya_$ll#g0h$#ddw@1gu5=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["mulberry-fashion-dev.us-west-2.elasticbeanstalk.com",]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -81,37 +81,13 @@ WSGI_APPLICATION = "mulberry.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
-
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "ebdb",
-#         "USER": "mulberryDB",
-#         "PASSWORD": "Thankachan",
-#         "HOST": "awseb-e-udxburmppk-stack-awsebrdsdatabase-dn0do6huodi3.cr6eummm6ipo.us-west-2.rds.amazonaws.com",
-#         "PORT": 5432,
-#     }
-# }
-
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ["RDS_DB_NAME"],
-        "USER": os.environ["RDS_USERNAME"],
-        "PASSWORD": os.environ["RDS_PASSWORD"],
-        "HOST": os.environ["RDS_HOSTNAME"],
-        "PORT": os.environ["RDS_PORT"],
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
